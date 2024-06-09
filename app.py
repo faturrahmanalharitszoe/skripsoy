ad97715fa073645acbfa056af450f6dec9eb198b
Faturrahman Alharitszoe <72615238+faturrahmanalharitszoe@users.noreply.github.com>
2024-06-09 14:28:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -104,8 +104,3 @@ def detect_objects():
     }
 
     return jsonify(response)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-
