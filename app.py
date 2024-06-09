0120d18099d0722a33db15ae73c1ea192120d7ea
Faturrahman Alharitszoe <72615238+faturrahmanalharitszoe@users.noreply.github.com>
2024-06-09 15:23:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import cv2
 import torch
 import numpy as np
 import os
-
 import pathlib
+
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
@@ -20,19 +20,15 @@ ocr_model = torch.hub.load('ultralytics/yolov5', 'custom', path=ocr_model_path)
 def detect_and_crop_objects(image, conf_thres=0.25, iou_thres=0.45, imgsz=640):
     # Perform inference
     results = crop_model(image)
-
     # Apply non-max suppression to get filtered results
     results = results.xyxy[0].cpu().numpy()  # Get the xyxy format results
     results = results[results[:, 4] > conf_thres]  # Filter by confidence threshold
-
     cropped_images = []
-
     # Process detections
     for x1, y1, x2, y2, conf, cls in results:
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         cropped_img = image[y1:y2, x1:x2]
         cropped_images.append(cropped_img)
-
     return cropped_images
 
 # Define a directory to save cropped images
@@ -46,11 +42,9 @@ if not os.path.exists(SAVE_DIR):
 def detect_and_crop():
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'})
-
     image_file = request.files['image']
     image_np = np.frombuffer(image_file.read(), np.uint8)
     image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-
     cropped_images = detect_and_crop_objects(image)
 
     result = []
@@ -65,7 +59,7 @@ def detect_and_crop():
 def detect_objects():
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'})
-
+    
     # Load the image from the request
     image_file = request.files['image']
     image_np = np.frombuffer(image_file.read(), np.uint8)
@@ -104,3 +98,7 @@ def detect_objects():
     }
 
     return jsonify(response)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
