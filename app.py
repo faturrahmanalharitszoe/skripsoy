82b2ccf5486cd986ee1ee56e30d5984a52c09ca1
Faturrahman Alharitszoe <72615238+faturrahmanalharitszoe@users.noreply.github.com>
2024-06-25 23:41:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify, Response
 import cv2
 import torch
 import numpy as np
+import os
 import base64
+import io
+from PIL import Image
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
-
 crop_model_path = '/app/crop.pt'
 ocr_model_path = '/app/ocr.pt'
-
 # Load the YOLOv5 models from Ultralytics
 crop_model = torch.hub.load('ultralytics/yolov5', 'custom', path=str(crop_model_path))
 ocr_model = torch.hub.load('ultralytics/yolov5', 'custom', path=str(ocr_model_path))
-
 def detect_and_crop_objects(image, conf_thres=0.25, iou_thres=0.45, imgsz=640):
     # Perform inference
     results = crop_model(image)
@@ -28,7 +29,6 @@ def detect_and_crop_objects(image, conf_thres=0.25, iou_thres=0.45, imgsz=640):
         cropped_img = image[y1:y2, x1:x2]
         cropped_images.append(cropped_img)
     return cropped_images
-
 def perform_ocr_on_image(image):
     # Preprocessing steps
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -36,8 +36,16 @@ def perform_ocr_on_image(image):
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     img_clahe = clahe.apply(img_gray)
     img_clahe = cv2.cvtColor(img_clahe, cv2.COLOR_GRAY2BGR)
-    return "OCR_RESULT_PLACEHOLDER"
 
+def detect_objects():
+    # Sort the filtered results horizontally by the x-axis of the bounding box
+    sorted_filtered_results = filtered_results[filtered_results[:, 0].argsort()]
+
+    # Extract class names in the order of sorted detections by X1
+    sorted_class_names = [ocr_model.names[int(cls)] for cls in sorted_filtered_results[:, -1]]
+    # Combine class names into a single string, preserving their left-to-right order in the image
+    combined_classes_text = ''.join(sorted_class_names)
+    return combined_classes_text
 @app.route('/detect_and_ocr', methods=['POST'])
 def detect_and_ocr():
     if 'image' not in request.files:
@@ -45,25 +53,19 @@ def detect_and_ocr():
     image_file = request.files['image']
     image_np = np.frombuffer(image_file.read(), np.uint8)
     image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-
     # Detect and crop objects
     cropped_images = detect_and_crop_objects(image)
-
     result = []
     for cropped_img in cropped_images:
         # Perform OCR on the cropped image
         ocr_result = perform_ocr_on_image(cropped_img)
-
         # Encode the cropped image in base64 format
         _, buffer = cv2.imencode('.jpg', cropped_img)
         img_base64 = base64.b64encode(buffer).decode('utf-8')
-
         result.append({
             'cropped_image': img_base64,
             'ocr_result': ocr_result
         })
-
     return jsonify(result)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
